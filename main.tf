a20c7db38f2fa8829705098256f344284e42d31b
Karthik S <karthik@New-Mini.local>
2020-08-26 00:10:56 +0100

    delete provider

--- main.tf
+++ main.tf
@@ -10,15 +10,6 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  version = "2.0.0"
-
-  features {
-
-  }
-}
-
-
 resource "azurerm_resource_group" "mygithubtest" {
   name = "mygithub_resource_group"
   location = "eastus"
