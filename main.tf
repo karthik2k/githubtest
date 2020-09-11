5caaa8e8d5fd296be58603cd5084c1e32ee9955b
Karthik S <karthik@New-Mini.local>
2020-09-11 22:53:27 +0100

    start

--- main.tf
+++ main.tf
@@ -10,11 +10,10 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  version = "=2.0.0"
-}
-
 resource "azurerm_resource_group" "mygithubtest" {
   name = "mygithub_resource_group"
   location = "eastus"
+    tags {
+      name = "Karthik Si"
+      }
 }
