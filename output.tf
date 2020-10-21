dfc985601841eab1b25baa98083837d44c4b8cc8
Karthik S <karthik@New-Mini.local>
2020-10-21 18:46:33 +0100

    output added2

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "azurerm_storage_account" {
   value = azurerm_storage_account.MyGithubTest.name
 }
 
-output "azurerm_network_interface" MyGithubTest {
+output "azurerm_network_interface" {
   value = azurerm_network_interface.MyGithubTest.namea
 }
 
