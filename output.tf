40d1e088a72355e733dcee9ecbe8da3af7310dec
Karthik S <karthik@New-Mini.local>
2020-10-21 22:31:05 +0100

    Rebuild

--- output.tf
+++ output.tf
@@ -5,4 +5,9 @@ output "azurerm_storage_account" {
 output "azurerm_network_interface" {
   value = azurerm_network_interface.MyGithubTest.name
 }
-
+output "azure_dns_a_record" {
+  value = "azurerm_dns_zone.MyGithubTest.name"
+}
+output "azurerm_public_ip" {
+  value = "azurerm_public_ip.MyGithubTest.id"
+}
